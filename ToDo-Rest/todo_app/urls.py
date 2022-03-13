d3b4c233e2dd1931151f937eb93db83650db804e
pinballtec <daniilboiko99@gmail.com>
2022-03-13 19:14:26 +0100

    added delete and edit buttons(frontend has been set up)

---
+++ ToDo-Rest/todo_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.apiOverview, name='api-overview'),
+    path('task-list/', views.task_list, name='tasks'),
+    path('task-detail/<str:pk>/', views.task_detail, name='tasks-detail'),
+    path('task-create/', views.task_create, name='tasks-create'),
+    path('task-update/<str:pk>/', views.task_update, name='tasks-update'),
+    path('task-delete/<str:pk>/', views.task_delete, name='tasks-delete')
+]
